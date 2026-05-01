40d75358d0b3d24e8bf5a484fd39c734622b8662
Opurbo007 <opurboujjol@gmail.com>
2026-05-01 21:37:02 +0600

    proxy updated

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,12 +1,13 @@
+import { auth } from "@/lib/auth";
 import { NextRequest, NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
 
-// This is proxy
 export const config = {
   matcher: ["/((?!_next|favicon.ico|api/auth).*)"],
 };
 
 export async function proxy(req: NextRequest) {
+  const session = await auth();
+
   const { pathname } = req.nextUrl;
 
   if (
@@ -17,12 +18,7 @@ export async function proxy(req: NextRequest) {
     return NextResponse.next();
   }
 
-  const token = await getToken({
-    req,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
-
-  if (!token) {
+  if (!session?.user) {
     const url = new URL("/login", req.url);
     url.searchParams.set("callbackUrl", req.nextUrl.href);
     return NextResponse.redirect(url);
